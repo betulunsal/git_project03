09e1f6b5aea28472aeff7c9f9f3fe77037eb386e
betul unsal <btulunsal@gmail.com>
2020-03-16 19:22:28 -0400

    my first commit

---
+++ src/gitDay4Package/GitDay4.java
@@ -0,0 +1,18 @@
+package gitDay4Package;
+
+public class GitDay4 {
+
+	
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Local first commit master branch");
+		
+		
+		
+		
+		
+		
+	}
+}
